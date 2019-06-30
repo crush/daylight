8c1a311cb498a265332ccdbdbf8c55bc8851ff31
Crush <crush.dev.tf@gmail.com>
2019-06-30 01:01:05 -0400

    Not using templated

--- daylight/routes.py
+++ daylight/routes.py
@@ -1,9 +1,7 @@
-from flask import render_template
-
 from daylight import app
 
 
 @app.route("/")
 def index():
     app.logger.warning("sample message")
-    return render_template("index.html")
+    return "Hello from daylight!"
