19d3a0f1f3efee769a4cc133546d8b1218cebda1
Crush <crush.dev.tf@gmail.com>
2019-07-14 18:00:33 -0400

    Starting to define a template for request handlers

--- daylight/routes/register.py
+++ daylight/routes/register.py
@@ -1,6 +1,30 @@
+from dataclasses import dataclass
+import json
+from typing import Optional
+
+from flask import request
+
 from daylight import app
 
 
-@app.routes('/users/register', methods=['POST'])
+@dataclass
+class _Request:
+
+    username: str
+    email: str
+    password: str
+    account_type: str
+
+
+@dataclass
+class _Response:
+    error: Optional[str]
+    data: None
+
+
+@app.route('/users/register', methods=['POST'])
 def register():
-    return 'Not implemented'
+    req = _Request(**request.json)
+    res = _Response('test error')
+
+    return json.dumps(res)
