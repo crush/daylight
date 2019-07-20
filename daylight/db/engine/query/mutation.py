5ce694ae4dac6e5b8d464d5dcfac269b973f6c63
Crush <crush.dev.tf@gmail.com>
2019-07-20 18:10:29 -0400

    First step of implementing all the query interfaces

--- daylight/db/engine/query/mutation.py
+++ daylight/db/engine/query/mutation.py
@@ -5,9 +5,10 @@ execute against the database.
 
 from dataclasses import dataclass
 import enum
-from typing import Any, List
+from typing import Any, List, Union
 
 import daylight.db.engine.query.security as security
+import daylight.db.models as models
 
 
 class MutationId(enum.Enum):
@@ -16,6 +17,17 @@ class MutationId(enum.Enum):
     '''
     
     __REGISTER_USER = '__register_user__'
+    __RESET_PASSWORD = '__reset_password__'
+    __DELETE_USER = '__delete_user__'
+    __MATCH_USERS = '__match_users__'
+    __UNMATCH = '__unmatch__'
+    __UPDATE_PROFILE = '__update_user_profile__'
+    __SET_TAGS = '__set_tags__'
+    __UPLOAD_PHOTO = '__upload_photo__'
+    __REMOVE_PHOTO = '__remove_photo__'
+
+
+AccountType = Union[models.WomanFemmeAccountType, models.ManMascAccountType]
 
 
 @dataclass
@@ -28,9 +40,93 @@ class Mutation:
     parameters: List[Any]
 
 
-def register_user(email: str, password: str) -> Mutation:
-    '''Create a `Query` that will insert a new user into the database.
+def register_user(
+        email: str,
+        password: str,
+        account_type: AccountType,
+        ) -> Mutation:
+    '''Create a `Mutation` that will insert a new user into the database.
     '''
 
     password = security.hash_password(password)
-    return Mutation(MutationId.__REGISTER_USER, args=[email, password])
+    return Mutation(MutationId.__REGISTER_USER, [email, password, account_type])
+
+
+def reset_password(updated_user: models.User) -> Mutation:
+    '''Create a `Mutation` to update a user's password with a secure hash of a
+    new password.
+    '''
+
+    new_password = security.hash_password(new_password)
+    return Mutation(MutationId.__RESET_PASSWORD, [user_id, new_password])
+
+
+def delete_user_account(user: models.User) -> Mutation:
+    '''Create a `Mutation` that will completely delete a user account
+    and all related data.
+    '''
+
+    return Mutation(MutationId.__DELETE_USER, [user_id])
+
+
+def establish_match(from_user_id: int, to_user_id: int) -> Mutation:
+    '''Create a `Mutation` that creates a match between two users.
+    '''
+
+    return Mutation(MutationId.__MATCH_USERS, [from_user_id, to_user_id])
+
+
+def unmatch(match: models.Match) -> Mutation:
+    '''Create a `Mutation` that will delete a match between two users.
+    '''
+
+    return Mutation(MutationId.__UNMATCH, [from_user_id, to_user_id])
+
+
+def update_user_profile(updated_profile: models.Profile) -> Mutation:
+    '''Create a `Mutation` that updates all of the fields in a user's profile.
+    '''
+
+    return Mutation(MutationId.__UPDATE_PROFILE, [updated_profile])
+
+
+def set_user_profile_tags(
+        user: models.User,
+        tags: List[models.Tag]
+        ) -> Mutation:
+    '''Create a `Mutation` that will set the tags associated with a user's
+    profile.
+    '''
+
+    return Mutation(MutationId.__SET_TAGS, [user, tags])
+
+
+def upload_photo_to_profile(
+        profile: models.Profile,
+        photo_src: str
+        ) -> Mutation:
+    '''Create a `Mutation` that will relate a new photo to a user's profile.
+    '''
+
+    return Mutation(MutationId.__UPLOAD_PHOTO, [profile, photo_src])
+
+
+def remove_photo_from_profile(
+        profile: models.Profile,
+        photo: models.Photo
+        ) -> Mutation:
+    '''Create a `Mutation` that will remove a photo from a user's profile.
+    '''
+    
+    return Mutation(MutationId.__REMOVE_PHOTO, [profile, photo])
+
+
+def set_profile_picture(
+        profile: models.Profile,
+        photo: models.Photo
+        ) -> Mutation:
+    '''Create a `Mutation` to set a particular photo as a user's profile
+    picture.
+    '''
+
+    return Mutation(MutationId.__SET_PROFILE_PIC, [profile, photo])
--- daylight/db/engine/query/query.py
+++ daylight/db/engine/query/query.py
@@ -13,6 +13,11 @@ class QueryId(enum.Enum):
     '''
 
     __RETRIEVE_USER_BY_ID = '__retrieve_user_by_id__'
+    __RETRIEVE_MATCHES_FOR_USER = '__retrieve__matches_for_user__'
+    __RETRIEVE_USER_PROFILE = '__retrieve_user_profile__'
+    __RETRIEVE_TAGS = '__retrieve_profile_tags__'
+    __RETRIEVE_PHOTOS = '__retrieve_profile_photos__'
+    __RETRIEVE_TYPED_PROFILE = '__retrieve_typed__profile__'
 
 
 @dataclass
@@ -25,8 +30,49 @@ class Query:
     parameters: List[Any]
 
 
-def retrieve_user(user_id: str) -> Query:
+AccountType = Union[models.WomanFemmeAccountType, models.ManMascAccountType]
+
+
+def retrieve_user(user_id: int) -> Query:
     '''Create a `Query` capble of searching for a specific user.
     '''
 
-    return Query(QueryId.__RETRIEVE_USER_BY_ID, args=[user_id])
+    return Query(QueryId.__RETRIEVE_USER_BY_ID, [user_id])
+
+
+def retrieve_matches_for_user(user: models.User) -> Query:
+    '''Create a `Query` to fetch all matches a user has.
+    '''
+
+    return Query(QueryId.__RETRIEVE_MATCHES_FOR_USER, [user])
+
+
+def retrieve_user_profile(user: models.User) -> Query:
+    '''Create a `Query` that retrieves the profile information for a user.
+    '''
+
+    return Query(QueryId.__RETRIEVE_USER_PROFILE, [user])
+
+
+def retrieve_user_tags(user: models.User) -> Query:
+    '''Create a `Query` that will retrieve the tags describing a user's
+    profile.
+    '''
+
+    return Query(QueryId.__RETRIEVE_TAGS, [user])
+
+
+def retrieve_user_photos(user: models.User) -> Query:
+    '''Create a `Query` to retrieve the photos associated with a user's
+    profile.
+    '''
+
+    return Query(QueryId.__RETRIEVE_PHOTOS, [user])
+
+
+def retrieve_typed_profile(profile: models.Profile) -> Query:
+    '''Create a `Query` that will retrieve the extra profile information
+    associated with each of the different types of profiles supported.
+    '''
+
+    return Query(QueryId.__RETRIEVE_TYPED_PROFILE, [profile])
