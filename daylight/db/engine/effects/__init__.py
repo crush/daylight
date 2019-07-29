f6914e869f8b0398451d1506c35b6c94d6ffb41a
Crush <crush.dev.tf@gmail.com>
2019-07-28 21:03:36 -0400

    Just export everything from sub-modules presenting an API

--- daylight/db/engine/effects/__init__.py
+++ daylight/db/engine/effects/__init__.py
@@ -1,25 +1,7 @@
 from daylight.db.engine.effects.state import State
-
-from daylight.db.engine.effects.likes import\
-        revoke_like,\
-        send_like
-from daylight.db.engine.effects.matches import\
-        establish_match,\
-        retrieve_matches,\
-        unmatch
-from daylight.db.engine.effects.photos import\
-        delete_photo,\
-        retrieve_photos,\
-        set_profile_pic,\
-        upload_photo
-from daylight.db.engine.effects.profiles import\
-        retrieve_profile,\
-        update_profile
-from daylight.db.engine.effects.tags import\
-        retrieve_tags,\
-        set_tags
-from daylight.db.engine.effects.users import\
-        create_user,\
-        delete_user,\
-        reset_password,\
-        retrieve_user
+from daylight.db.engine.effects.likes import *
+from daylight.db.engine.effects.matches import *
+from daylight.db.engine.effects.photos import *
+from daylight.db.engine.effects.profiles import *
+from daylight.db.engine.effects.tags import *
+from daylight.db.engine.effects.users import *
